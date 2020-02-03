de401c6da56198b6a59d951dfd935576e2c7f4ec
Richard Preen <rpreen@gmail.com>
2020-02-03 00:12:05 +0000

    update docs

--- xcs__multi__step_8c.js
+++ xcs__multi__step_8c.js
@@ -1,5 +1,6 @@
 var xcs__multi__step_8c =
 [
+    [ "xcs_multi_decision", "xcs__multi__step_8c.html#af2eaa15cba52d29fa3630218d9780a6a", null ],
     [ "xcs_multi_step_exp", "xcs__multi__step_8c.html#acc29d4a55dbd56c16ea5f19f15a7c677", null ],
     [ "xcs_multi_trial", "xcs__multi__step_8c.html#ad50c415970b4bfc42f469fb38c5e68fa", null ]
 ];
--- xcs__multi__step_8h.js
+++ xcs__multi__step_8h.js
@@ -1,4 +1,5 @@
 var xcs__multi__step_8h =
 [
+    [ "xcs_multi_decision", "xcs__multi__step_8h.html#af2eaa15cba52d29fa3630218d9780a6a", null ],
     [ "xcs_multi_step_exp", "xcs__multi__step_8h.html#acc29d4a55dbd56c16ea5f19f15a7c677", null ]
 ];
